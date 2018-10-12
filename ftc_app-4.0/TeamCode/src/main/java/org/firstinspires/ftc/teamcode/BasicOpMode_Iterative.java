aefb6074b18ce0c4e6107840fa394c7bec0e122e
Elise Harrison <eharrison@gannacademy.org>
2018-10-12 11:07:47 -0400

    re-enable teleop and suto programs to DS can see them

--- ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -51,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
+
 public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
